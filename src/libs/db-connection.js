01d0c68ae7540b3c1286e821487bb459aaa622f9
PedroAlbertoAguirre <pedro.aguirre.1995@gmail.com>
2019-12-18 00:12:05 -0300

    mlab ha sido agregado

--- src/libs/db-connection.js
+++ src/libs/db-connection.js
@@ -4,7 +4,7 @@ let db; //variable
 
 module.exports = function Connection(){
     if(!db){
-        db = mongoose.createConnection('mongodb://localhost/crud-example'); //me conecto a la base de datos
+        db = mongoose.createConnection('mongodb+srv://PedroAguirre:peters-77@cluster0-vos6s.gcp.mongodb.net/test?retryWrites=true&w=majority'); //me conecto a la base de datos
     
     }
     return db;
